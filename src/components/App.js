83511c3d9568ee4fc5bd3ca2b5abf969026bf71d
Nicholas Tai <ntai@berkeley.edu>
2020-03-02 00:31:38 -0800

    brh

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,7 @@ function App() {
 
 let groups = [];
 
-// for (let id in People) {
-//   groups.push()
-//   console.log(People[id]["name"]);
-// }
+console.log("hello");
 
 for (let index = 0; index < Object.keys(People).length; index++) {
   groups.push(
